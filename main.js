2217eb85d71dd2a9463f8d587fd66ce8a0d432fb
Sakinaa782 <sashotrupa@hotmail.com>
2022-06-29 21:45:46 +0300

    Finishing JS part

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ textareaElement.focus();
 
 textareaElement.addEventListener('keyup', (e) =>{
     createTags(e.target.value)
+
+    if(e.key === 'Enter' ){
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10);
+        randomPick();
+    }
 })
 
 function createTags(input){
@@ -22,4 +29,43 @@ function createTags(input){
         tagsElement.appendChild(tagEl)
     })
 
+}
+function randomPick(){
+   const times = 10;
+
+   const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+
+        highlightTag(randomTag);
+
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+   }, 100);
+
+
+
+   setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+
+            highlightTag(randomTag)
+        }, 100);
+   }, times * 100);
+}
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag');
+
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+function highlightTag(tag){
+    tag.classList.add('highlight');
+}
+function unHighlightTag(tag){
+    tag.classList.remove('highlight');
 }
